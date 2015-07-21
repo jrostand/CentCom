95d719bc47baef25d9078ec8f92e87d2ffc0f5fa
Julien Rostand <julien@rostand.net>
2015-07-20 20:53:30 -0500

    Start of RSS reader UI

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require './config/initialize'
 
+set :root, __dir__
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
+end
+
+get '/' do
+  @stories = Story.where(read: false).all
+  slim :index
 end
