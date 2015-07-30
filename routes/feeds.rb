ce1c375d340a468597b0c4cc1e887cfe72312974
Julien Rostand <julien@rostand.net>
2015-07-29 21:42:00 -0500

    Feed admin

--- routes/feeds.rb
+++ routes/feeds.rb
@@ -1,3 +1,50 @@
+get '/feeds' do
+  @categories = FeedCategory.eager(:feeds).order(Sequel.asc(:name)).all
+
+  slim :'feeds/index'
+end
+
+get '/feeds/new' do
+  @categories = FeedCategory.order(Sequel.asc(:name)).all
+  @feed = Feed.new
+
+  slim :'feeds/new'
+end
+
+post '/feeds/new' do
+  @feed = Feed.new params['feed']
+
+  begin
+    @feed.save
+    FeedReaderWorker.perform_async @feed.id
+
+    redirect '/feeds'
+  rescue
+    @categories = FeedCategory.order(Sequel.asc(:name)).all
+
+    slim :'feeds/new', locals: { form_errors: @feed.errors }
+  end
+end
+
+get '/feeds/:id/edit' do |feed_id|
+  @categories = FeedCategory.order(Sequel.asc(:name)).all
+  @feed = Feed[feed_id]
+
+  slim :'feeds/edit'
+end
+
+post '/feeds/:id/edit' do |feed_id|
+  @feed = Feed[feed_id]
+
+  begin
+    @feed.update params['feed']
+    redirect '/feeds'
+  rescue
+    @categories = FeedCategory.order(Sequel.asc(:name)).all
+    slim :'feeds/edit', locals: { form_errors: @feed.errors }
+  end
+end
+
 post '/feeds/refresh' do
   AllFeedsWorker.perform_async
   render_json message: 'OK'
