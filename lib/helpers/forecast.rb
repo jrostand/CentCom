bf8bdc7bdf5e6bf81e69d2a091c6bae3ae45b5b6
Julien Rostand <jrostand@users.noreply.github.com>
2015-10-26 22:08:48 -0500

    Backend changes to prep for React weather module

---
+++ lib/helpers/forecast.rb
@@ -0,0 +1,47 @@
+module ForecastFormatter
+  def json_forecast(fc)
+    current = fc.currently
+
+    {
+      currentIcon: icon_classes(current.icon),
+      currentTemp: current.temperature.round(1),
+      currentPressure: current.pressure.round,
+      currentWind: {
+        speed: current.windSpeed,
+        direction: current.windBearing
+      },
+      forecast: forecast_days(fc.daily.data)
+    }
+  end
+
+  private
+
+  def forecast_days(days)
+    days.first(5).map do |day|
+      {
+        label: Time.at(day.time).strftime('%a').downcase,
+        icon: icon_classes(day.icon),
+        min: day.temperatureMin.round(1),
+        max: day.temperatureMax.round(1)
+      }
+    end
+  end
+
+  def icon_classes(icon)
+    case icon
+      when 'clear-day' then %w(icon-sun)
+      when 'clear-night' then %w(icon-moon)
+      when 'cloudy' then %w(icon-cloud)
+      when 'fog' then %w(icon-basecloud icon-mist)
+      when 'hail' then %w(icon-hail)
+      when 'partly-cloudy-day' then %w(icon-cloud icon-sunny)
+      when 'partly-cloudy-night' then %w(icon-cloud icon-night)
+      when 'rain' then %w(icon-basecloud icon-showers)
+      when 'sleet' then %w(icon-basecloud icon-sleet)
+      when 'snow' then %w(icon-basecloud icon-snow)
+      when 'thunderstorm' then %w(icon-basecloud icon-thunder)
+      when 'wind' then %w(icon-basecloud icon-sunny icon-windy)
+      else %w(fa fa-alert)
+    end
+  end
+end
--- routes/api/weather.rb
+++ routes/api/weather.rb
@@ -1,12 +1,14 @@
+include ForecastFormatter
+
 get '/api/weather' do
   forecast = ForecastIO.forecast(
     CentCom::Settings.forecast_lat,
     CentCom::Settings.forecast_long,
     params: {
-      exclude: 'minutely,flags',
+      exclude: 'hourly,minutely,flags',
       units: 'si'
     }
   )
 
-  render_json forecast
+  render_json json_forecast(forecast)
 end
